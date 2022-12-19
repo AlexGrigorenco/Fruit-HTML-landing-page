3db79b849e11b30192adf2055534c71284d5742c
Alex <alpsnhandy@gmail.com>
2022-12-19 12:10:47 +0200

    close menu to click on links and buttons

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -3,7 +3,25 @@ const menu = document.querySelector('.menu__wrapper');
 const body = document.querySelector('body')
 
 
+
+const menuLinks = menu.querySelectorAll('a')
+const menuBttn = menu.querySelectorAll('button')
+const menuClickObject = [...menuLinks, ...menuBttn]
+
+console.log(menuClickObject)
+
 mobileBTN.onclick = function(){
+  toggleMenu()
+
+  menuClickObject.forEach(elem => {
+    elem.addEventListener('click', () => {
+      
+      document.querySelector('.menu__wrapper__mobile') ? toggleMenu() : null
+    })
+  })
+}
+
+function toggleMenu(){
   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
   menu.classList.toggle('menu__wrapper__mobile');
   body.classList.toggle('no-scroll');
